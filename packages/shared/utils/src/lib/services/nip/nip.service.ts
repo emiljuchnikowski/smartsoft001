2759070549cb06d87363f6aed3168b8650562aa2
Emil Juchnikowski <emil@juchnikowski.com>
2023-10-06 09:19:31 +0200

    chore(shared): fix test import services

--- packages/shared/utils/src/lib/services/nip/nip.service.ts
+++ packages/shared/utils/src/lib/services/nip/nip.service.ts
@@ -8,7 +8,7 @@ export class NipService {
         if(typeof nip !== 'string')
             return false;
 
-        nip = nip.replace(/[\ \-]/gi, '');
+        nip = nip.replace(/[ \-]/gi, '');
 
         const weight = [6, 5, 7, 2, 3, 4, 5, 6, 7];
         let sum = 0;
