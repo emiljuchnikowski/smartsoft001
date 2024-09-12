4da519170737c15a1998bb18e39a53148aaebfb3
Emil Juchnikowski <emil@juchnikowski.com>
2024-09-12 23:25:16 +0200

    ci(nx): scope validation
    Refs: FRA-4

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,27 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+const fs = require('fs');
+const path = require('path');
+
+const getDirectories = (source) =>
+    fs
+        .readdirSync(source, { withFileTypes: true })
+        .filter((dirent) => dirent.isDirectory())
+        .map((dirent) => dirent.name);
+
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-enum': async () => {
+            return [
+                2,
+                'always',
+                [
+                    ...getDirectories(path.join(__dirname, 'packages'))
+                        .filter((name) => name.indexOf('-e2e') === -1),
+                    'nx',
+                    'github',
+                    'docker',
+                ],
+            ];
+        },
+    },
+};
