1e125d33e5153b4edcc74b1fe319fc8347f0a54d
Emil Juchnikowski <emil@juchnikowski.com>
2025-05-02 10:51:43 +0200

    build(nx): tailwind configuration

---
+++ packages/shared/angular/.storybook/preview.ts
@@ -0,0 +1,36 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  stories: ['../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling-webpack', // 👈🏽 This part
+      options: {
+        rules: [
+          {
+            test: /^globals\.scss$/,
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                options: { importLoaders: 1 },
+              },
+              {
+                loader: 'postcss-loader',
+                options: { implementation: require.resolve('postcss') },
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
+  framework: {
+    name: '@storybook/angular',
+    options: {},
+  },
+};
+
+export default config;
