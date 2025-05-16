45fc7768edbed69716981eb2e33d0bb1bb40002e
cookie_lobster <37004245+AndriiBilych@users.noreply.github.com>
2025-05-16 14:20:01 +0100

    FRA-6 (#9)
    * fix(shared): kod do paczek fb i google
    
    ref: FRA-6
    
    * feat(shared): testy i konfiguracja paczek fb i google
    
    ref: FRA-6
    
    * fix(shared): nx formatting
    
    ref: FRA-6
    
    * fix(shared): ignore dependencies nestjs
    
    ref: FRA-6
    
    * fix(shared): nx formatting
    
    ref: FRA-6

---
+++ packages/shared/fb/README.md
@@ -0,0 +1,24 @@
+# 📦 @smartsoft001/fb
+
+![npm](https://img.shields.io/npm/v/@smartsoft001/fb) ![downloads](https://img.shields.io/npm/dm/@smartsoft001/fb)
+
+## 🚀 Usage
+
+`npm i @smartsoft001/fb`
+
+## 🛠️ Services & Methods
+
+### FbService
+
+Methods:
+
+<table>
+    <tr>
+        <td>getUserId</td>
+        <td>Makes a GET request to Facebook's Graph API to fetch the current user's profile</td>
+    </tr>
+    <tr>
+        <td>getData</td>
+        <td>Makes a GET request but specifically requests both email and ID fields</td>
+    </tr>
+</table>
---
+++ packages/shared/google/src/lib/google.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+
+@Injectable()
+export class GoogleService {
+  constructor(private http: HttpService) {}
+
+  async getUserId(token: string): Promise<string> {
+    const { data } = await this.http
+      .get(
+        'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=' + token,
+      )
+      .toPromise();
+
+    return data.user_id;
+  }
+
+  async getData(token: string): Promise<{ email; id }> {
+    const { data } = await this.http
+      .get(
+        'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=' + token,
+      )
+      .toPromise();
+
+    return {
+      id: data.user_id,
+      email: data.email,
+    };
+  }
+}
