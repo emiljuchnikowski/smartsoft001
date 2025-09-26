b2b527df9fd76c6a6487ace093b94f00daad744a
Emil Juchnikowski <emil@juchnikowski.com>
2025-09-26 12:38:20 +0200

    refactor(crud): improve effect handling and reducer logic

--- packages/crud/shell/angular/src/lib/+state/crud.reducer.ts
+++ packages/crud/shell/angular/src/lib/+state/crud.reducer.ts
@@ -100,32 +100,33 @@ const crudReducer = (
         links: null,
       };
 
-    case `[${entity}] Read Success`:
-      let list: any[] = [];
+    case `[${entity}] Read Success`: {
+        let list: any[] = [];
 
-      if (
-        action.filter &&
-        action.filter.offset &&
-        state.list &&
-        action.filter.paginationMode !== PaginationMode.singlePage
-      ) {
-        state.list.forEach((i) => {
-          list.push(i);
-        });
+        if (
+            action.filter &&
+            action.filter.offset &&
+            state.list &&
+            action.filter.paginationMode !== PaginationMode.singlePage
+        ) {
+            state.list.forEach((i) => {
+                list.push(i);
+            });
 
-        list = [...list, ...action.result.data];
-      } else {
-        list = action.result.data;
-      }
+            list = [...list, ...action.result.data];
+        } else {
+            list = action.result.data;
+        }
 
-      return {
-        ...state,
-        loaded: true,
-        list,
-        totalCount: action.result.totalCount,
-        links: action.result.links,
-        error: null,
-      };
+        return {
+            ...state,
+            loaded: true,
+            list,
+            totalCount: action.result.totalCount,
+            links: action.result.links,
+            error: null,
+        };
+    }
 
     case `[${entity}] Clear`:
       return { ...initialState };
