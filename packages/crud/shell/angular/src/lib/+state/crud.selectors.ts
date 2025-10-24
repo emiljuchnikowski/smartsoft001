5339c62f4728d0dcedee961a85618ce578348cce
Emil Juchnikowski <emil@juchnikowski.com>
2025-10-24 23:18:41 +0200

    refactor(crud): handle optional chaining for state properties

--- packages/crud/shell/angular/src/lib/+state/crud.selectors.ts
+++ packages/crud/shell/angular/src/lib/+state/crud.selectors.ts
@@ -10,19 +10,19 @@ export const getCrudMultiSelected = (entity: string) =>
   createSelector(getCrudState(entity), (state) => state.multiSelected);
 
 export const getCrudList = (entity: string) =>
-  createSelector(getCrudState(entity), (state) => state.list);
+  createSelector(getCrudState(entity), (state) => state?.list);
 
 export const getCrudTotalCount = (entity: string) =>
-  createSelector(getCrudState(entity), (state) => state.totalCount);
+  createSelector(getCrudState(entity), (state) => state?.totalCount);
 
 export const getCrudLinks = (entity: string) =>
-  createSelector(getCrudState(entity), (state) => state.links);
+  createSelector(getCrudState(entity), (state) => state?.links);
 
 export const getCrudLoaded = (entity: string) =>
-  createSelector(getCrudState(entity), (state) => state.loaded);
+  createSelector(getCrudState(entity), (state) => state?.loaded);
 
 export const getCrudFilter = (entity: string) =>
   createSelector(getCrudState(entity), (state) => state?.filter);
 
 export const getCrudError = (entity: string) =>
-  createSelector(getCrudState(entity), (state) => state.error);
+  createSelector(getCrudState(entity), (state) => state?.error);
